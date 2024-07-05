e4c50d50503c326c5dbd19a48a5e6916591d1db9
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2024-07-05 19:02:57 +0300

    feat: add button component

---
+++ src/renderer/src/components/button.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+
+import { cn } from '@renderer/utils'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-xs font-medium focus-visible:outline-none cursor-default',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-zinc-600 text-white',
+        secondary: 'bg-white text-zinc-700 border border-zinc-300',
+        tertiary: 'bg-white text-zinc-600'
+      },
+      size: {
+        default: 'px-2 py-1',
+        icon: 'p-1'
+      }
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'default'
+    }
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./src/renderer/index.html', './src/renderer/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      xs: ['0.75rem', '1.125rem'],
+      sm: ['0.875rem', '1.25rem'],
+      base: ['1rem', '1.5rem']
+    },
     extend: {}
   },
   plugins: []
