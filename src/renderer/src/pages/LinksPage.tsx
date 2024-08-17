0f166b3378d472f0fbf7ea3de30289160141b677
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2024-08-17 16:39:31 +0300

    feat: add scroll area component

--- src/renderer/src/pages/LinksPage.tsx
+++ src/renderer/src/pages/LinksPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
 
-import useLinksStore from '@renderer/stores/LinksStore'
 import LinkCard from '@renderer/components/link-card'
+import { ScrollArea } from '@renderer/components/scroll-area'
+
+import useLinksStore from '@renderer/stores/LinksStore'
 
 const LinksPage = (): JSX.Element => {
   const { links, getLinks } = useLinksStore()
@@ -11,13 +13,13 @@ const LinksPage = (): JSX.Element => {
   }, [])
 
   return (
-    <main className="w-full h-full relative p-8 flex flex-col items-center overflow-auto">
+    <ScrollArea className="w-full h-full relative p-8 flex flex-col items-center overflow-auto">
       <div className="max-w-[45rem] w-full h-full flex flex-col gap-y-1">
         {links.map((link) => (
           <LinkCard key={link.id} link={link} />
         ))}
       </div>
-    </main>
+    </ScrollArea>
   )
 }
 
