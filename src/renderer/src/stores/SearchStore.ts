8d5bbf16a7ec02e4ba16398ccff010ade4244eab
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2024-11-27 17:02:20 +0300

    feat: add search functionality

---
+++ src/renderer/src/stores/SearchStore.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand'
+
+import Fuse from 'fuse.js'
+
+import useLinksStore from '@renderer/stores/LinksStore'
+import useNotesStore from '@renderer/stores/NotesStore'
+
+interface SearchState {
+  query: string
+  results: AllItems[]
+  handleQueryChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  search: () => void
+}
+
+const fuseOptions = {
+  keys: ['title', 'content']
+}
+
+let fuse = new Fuse<Link | Note>([], fuseOptions)
+
+const useSearchStore = create<SearchState>((set, get) => ({
+  query: '',
+  results: [],
+  handleQueryChange: (e): void => set({ query: e.target.value }),
+  search: (): void => {
+    const links = useLinksStore.getState().links
+    const notes = useNotesStore.getState().notes
+
+    fuse = new Fuse([...links, ...notes], fuseOptions)
+
+    const query = get().query
+    const results = fuse.search(query).map((result) => result.item)
+
+    set({ results })
+  }
+}))
+
+export default useSearchStore
