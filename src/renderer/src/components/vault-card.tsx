ae6faf7ddb41f26902334029ffe313b07e6c4d56
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2025-01-07 14:28:01 +0300

    feat: add vault opening functionality to vault card component

--- src/renderer/src/components/vault-card.tsx
+++ src/renderer/src/components/vault-card.tsx
@@ -1,8 +1,29 @@
+import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
 import { Vault, Ellipsis } from 'lucide-react'
 
 import { Button } from '@renderer/components/button'
 
+import useVaultStore from '@renderer/stores/VaultStore'
+
 const VaultCard = ({ vault }: { vault: Vault }): JSX.Element => {
+  const navigate = useNavigate()
+
+  const { setVaultStore } = useVaultStore()
+
+  const handleOpenVault = async (): Promise<void> => {
+    try {
+      const openedVault: Vault = await window.electron.ipcRenderer.invoke('open-vault', vault.path)
+
+      setVaultStore(openedVault.name, openedVault.path, openedVault.createdAt)
+
+      navigate('/all-items')
+
+      toast.success('Vault opened successfully.')
+    } catch (error) {
+      toast.error('Failed to open the vault.')
+    }
+  }
   return (
     <div className="flex items-center justify-between p-3 border border-zinc-200 rounded-md">
       <div className="flex items-center gap-x-2">
@@ -13,7 +34,9 @@ const VaultCard = ({ vault }: { vault: Vault }): JSX.Element => {
         <Button size="icon" variant="tertiary">
           <Ellipsis className="size-5 text-zinc-600" />
         </Button>
-        <Button variant="secondary">open</Button>
+        <Button variant="secondary" onClick={handleOpenVault}>
+          open
+        </Button>
       </div>
     </div>
   )
