88e882d9a56666fb0ae610135716d6bbc0b4e7dc
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2025-01-12 14:54:55 +0300

    feat: integrate tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./entrypoints/popup/index.html",
+    "./entrypoints/popup/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ["0.75rem", "1.125rem"],
+      sm: ["0.875rem", "1.25rem"],
+      base: ["1rem", "1.5rem"],
+      lg: ["1.125rem", "1.75rem"],
+      xl: ["1.25rem", "1.875rem"],
+      "2xl": ["1.5rem", "2rem"],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        md: "0px 2px 4px -2px rgba(24, 24, 27, 0.06), 0px 4px 8px -2px rgba(24, 24, 27, 0.10)",
+      },
+    },
+  },
+};
