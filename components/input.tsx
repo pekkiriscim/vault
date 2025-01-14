9bb4f053a5e4ec7fc5d562f9795ca889fcb31555
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2025-01-14 15:02:59 +0300

    feat: add link title and url input fields

---
+++ components/input.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import cn from "@/utils/cn";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex w-full h-[1.75rem] rounded-md border border-zinc-300 bg-zinc-50 px-2 py-1 text-sm text-zinc-900 placeholder:text-zinc-500 focus-visible:outline-none",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input };
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,9 +1,39 @@
+import vault from "@/assets/vault.svg";
+
+import { Label } from "@/components/label";
+import { Input } from "@/components/input";
 import { Button } from "@/components/button";
 
 function App() {
   return (
-    <main>
-      <Button>save</Button>
+    <main className="w-96">
+      <div className="flex items-start justify-between px-5 pt-5 pb-4 border-b border-zinc-200">
+        <div className="flex flex-col gap-y-0.5">
+          <h1 className="text-base font-medium text-zinc-900">Add link</h1>
+          <p className="text-sm text-zinc-600">mustafa’s vault</p>
+        </div>
+        <img src={vault} alt="vault" />
+      </div>
+      <div className="flex flex-col gap-y-6 px-5 pt-4 pb-5">
+        <div className="flex flex-col gap-y-3">
+          <div className="flex flex-col gap-y-1">
+            <Label htmlFor="title">title</Label>
+            <Input placeholder="link title" id="title" />
+          </div>
+          <div className="flex flex-col gap-y-1">
+            <Label htmlFor="url">url</Label>
+            <Input placeholder="link url" id="url" />
+          </div>
+        </div>
+        <div className="flex gap-x-2">
+          <Button variant="secondary" className="w-full">
+            cancel
+          </Button>
+          <Button variant="primary" className="w-full">
+            add
+          </Button>
+        </div>
+      </div>
     </main>
   );
 }
