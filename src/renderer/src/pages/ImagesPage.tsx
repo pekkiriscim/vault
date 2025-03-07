c6263bf2c7cc4158a71bedd7070868daf788923e
Mustafa Pekkirişci <pekkiriscim@gmail.com>
2025-03-07 22:10:29 +0300

    feat: add virtualization to images page

--- src/renderer/src/pages/ImagesPage.tsx
+++ src/renderer/src/pages/ImagesPage.tsx
@@ -1,21 +1,42 @@
-import { useEffect, memo } from 'react'
+import { useEffect, memo, useMemo } from 'react'
+
+import { Virtuoso } from 'react-virtuoso'
+
+import { useOutletContext } from 'react-router-dom'
 
 import ImageCard from '@renderer/components/image-card'
 
 import useImagesStore from '@renderer/stores/ImagesStore'
 
+import groupImages from '@renderer/utils/groupImages'
+
 const ImagesPage = (): JSX.Element => {
   const { images, getImages } = useImagesStore()
+
+  const scrollParent = useOutletContext<HTMLDivElement | null>()
 
   useEffect(() => {
     getImages()
   }, [])
 
+  const groupedImages = useMemo(() => groupImages(images, 3), [images])
+
   return (
-    <div className="w-full h-full max-w-3xl mx-auto px-9 pb-10 pt-8 grid grid-cols-3 gap-3">
-      {images.map((image) => (
-        <ImageCard key={image.id} image={image} />
-      ))}
+    <div className="w-full h-full max-w-3xl mx-auto px-9 pb-10 pt-8 flex flex-col">
+      <Virtuoso
+        data={groupedImages}
+        itemContent={(groupIndex, group) => {
+          return (
+            <div key={`group-${groupIndex}`} className="grid grid-cols-3 gap-x-3">
+              {group.map((image) => (
+                <ImageCard key={image.id} image={image} />
+              ))}
+            </div>
+          )
+        }}
+        customScrollParent={scrollParent || undefined}
+        className="virtualized-images-container w-full"
+      />
     </div>
   )
 }
